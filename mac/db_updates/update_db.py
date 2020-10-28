0c5c7500451414fe67ca400297c63d183c240d4e
Arpit Jain <arpitkjain@deloitte.com>
2020-10-28 19:43:47 +0530

    Database changed

--- mac/db_updates/update_db.py
+++ mac/db_updates/update_db.py
@@ -1,27 +1,20 @@
-import psycopg2
+import sqlite3
 from datetime import datetime
 
 
-conn = psycopg2.connect(
-    database="postgres",
-    user="postgres",
-    password="password",
-    host="127.0.0.1",
-    port="5432",
-)
-cursor = conn.cursor()
+conn = sqlite3.connect("cpq_code_compare.db")
 
 
 def create_batch(batch_id: str):
-    sql = """ INSERT INTO cpqCodeCompartor.status (batch_id, status, started, ended, error) VALUES (%s,%s,%s,%s,%s)"""
-    cursor.execute(sql, (batch_id, "In-Progress", datetime.now(), None, None))
+    sql = """ INSERT INTO BATCH_STATUS (BATCH_ID,STATUS,START_TIME,END_TIME,ERROR) VALUES (?,?,?,?,?)"""
+    conn.execute(sql, (batch_id, "In-Progress", datetime.now(), None, None))
     conn.commit()
     print("Database updated successfully........")
 
 
 def update_batch(batch_id: str, status: str, error: str = None):
     time = datetime.now()
-    sql = """Update cpqCodeCompartor.status set status = %s, ended = %s, error = %s where batch_id = %s"""
-    cursor.execute(sql, (status, time, error, batch_id))
+    sql = """Update BATCH_STATUS SET STATUS=?,END_TIME=?,ERROR=? WHERE BATCH_ID = ?"""
+    conn.execute(sql, (status, time, error, batch_id))
     print("Database updated successfully........")
     conn.commit()
